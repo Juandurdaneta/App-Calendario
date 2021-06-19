52bf81ff959be96c3c4bae3fe6f44f51ba68932f
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2021-06-19 17:43:03 -0400

    Mejorando la experiencia de usuario

--- src/main/java/servlets/EliminarUsuario.java
+++ src/main/java/servlets/EliminarUsuario.java
@@ -18,21 +18,16 @@ import controllers.ControlPerfiles;
 @WebServlet("/Eliminar")
 public class EliminarUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+   
     public EliminarUsuario() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("/public/views/usuarioEliminado.html");
+		rd.include(request, response);
 	}
 
 	/**
@@ -44,7 +39,8 @@ public class EliminarUsuario extends HttpServlet {
 		String username = (String) request.getSession().getAttribute("username");
 		PrintWriter s=response.getWriter(); 
 		if(usuarioAEliminar.equals(username) && ControlPerfiles.Eliminar(usuarioAEliminar)) {
-			response.sendRedirect("register");
+			RequestDispatcher rd = request.getRequestDispatcher("/public/views/usuarioEliminado.html");
+			rd.include(request, response);
 		}
 		else {
 			s.print("<script>window.alert('El nombre ingresado no coincide con el nombre de Usuario.')</script>");
